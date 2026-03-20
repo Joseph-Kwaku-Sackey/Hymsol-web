710e6b1bcf53dd02af14ed395de3835d566d8577
builvec <josephsackey002@gmail.com>
2026-03-20 02:50:15 +0000

    made home privacy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 export default function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/privacy" element={<PrivacyPolicy />} />
+				<Route path="/" element={<PrivacyPolicy />} />
+				{/* <Route path="/privacy" element={<PrivacyPolicy />} /> */}
 			</Routes>
 		</Router>
 	);
